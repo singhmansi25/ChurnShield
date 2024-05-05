77ea3502945e1380d8ac4aa53c004a44cb09c8f1
unknown <mansi@spottabl.com>
2024-05-05 21:35:29 +0530

    added files

--- ChurnShield/settings.py
+++ ChurnShield/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-18t5=m4s(w(s__yht&87&*i0zqn2lzrt#s4x$zgas++5rd6&7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
