d84d431d3cfc4b7a99e6d4b7b5f0eeec9573b7e8
unknown <mansi@spottabl.com>
2024-05-06 11:59:45 +0530

    changes in static

--- ChurnShield/settings.py
+++ ChurnShield/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -116,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-FAVICON_URL = 'images/favicon.ico'
+
+FAVICON_URL = 'static/images/favicon.ico'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 python manage.py migrate
+python manage.py collectstatic
 gunicorn --workers 4 --timeout 60 --bind=0.0.0.0 --chdir=/home/site/wwwroot ChurnShield.wsgi --access-logfile '-' --error-logfile '-'
